6ac9d8ee86335f00e1a282bc589d68418a33c5e8
juncaGlobal <juncajapan.inc@gmail.com>
2026-07-26 23:10:19 +0900

    Preserve existing GitHub OIDC provider tags

--- infra/aws/bootstrap/main.tf
+++ infra/aws/bootstrap/main.tf
@@ -173,6 +173,10 @@ resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [var.github_oidc_thumbprint]
+
+  lifecycle {
+    ignore_changes = [tags, tags_all]
+  }
 }
 
 resource "aws_iam_role" "deployment" {
