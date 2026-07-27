2d0ece49ae2376586538a6fc3270abdc0b088a74
juncaGlobal <juncajapan.inc@gmail.com>
2026-07-27 15:43:14 +0900

    style(terraform): normalize public testnet formatting

--- infra/aws/public-testnet/main.tf
+++ infra/aws/public-testnet/main.tf
@@ -320,9 +320,9 @@ resource "aws_security_group" "endpoints" {
   vpc_id = aws_vpc.testnet.id
 
   ingress {
-    protocol  = "tcp"
-    from_port = 443
-    to_port   = 443
+    protocol        = "tcp"
+    from_port       = 443
+    to_port         = 443
     security_groups = [aws_security_group.validator.id]
   }
 }
