64d04aeef233a7c7681338f4dd0da042820f746d
juncaGlobal <juncajapan.inc@gmail.com>
2026-08-05 00:16:28 +0900

    Activate reviewed composite root and Browser app router source

--- infra/aws/jaios-root-news-router.js
+++ infra/aws/jaios-root-news-router.js
@@ -1,25 +1,33 @@
 import cf from 'cloudfront';
 
 const JAIOS_HOST = 'jaios-governance.org';
-const ROOT_ORIGIN = 'docs.jaios-governance.org';
+const GOVERNED_ORIGIN = 'docs.jaios-governance.org';
 const ROOT_ARTIFACT = '/jaios-root-news/index.html';
+const BROWSER_APP_ARTIFACT = '/jaios-browser-app/index.html';
+
+function routeToGovernedArtifact(request, artifact) {
+  request.uri = artifact;
+  cf.updateRequestOrigin({
+    domainName: GOVERNED_ORIGIN,
+    hostHeader: GOVERNED_ORIGIN,
+    sni: GOVERNED_ORIGIN,
+    allowedCertificateNames: [GOVERNED_ORIGIN]
+  });
+}
 
 function handler(event) {
   var request = event.request;
   var hostHeader = request.headers && request.headers.host;
   var host = hostHeader && hostHeader.value ? hostHeader.value.toLowerCase() : '';
   var isRoot = request.uri === '/' || request.uri === '/index.html';
+  var isBrowserApp = request.uri === '/browser/app' || request.uri === '/browser/app/';
   var isReadableMethod = request.method === 'GET' || request.method === 'HEAD';
 
   if (host === JAIOS_HOST) {
     if (isRoot && isReadableMethod) {
-      request.uri = ROOT_ARTIFACT;
-      cf.updateRequestOrigin({
-        domainName: ROOT_ORIGIN,
-        hostHeader: ROOT_ORIGIN,
-        sni: ROOT_ORIGIN,
-        allowedCertificateNames: [ROOT_ORIGIN]
-      });
+      routeToGovernedArtifact(request, ROOT_ARTIFACT);
+    } else if (isBrowserApp && isReadableMethod) {
+      routeToGovernedArtifact(request, BROWSER_APP_ARTIFACT);
     }
     return request;
   }
